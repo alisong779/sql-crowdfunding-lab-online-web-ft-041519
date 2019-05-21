3c81081b166e0d936a051639d7b89e5bcf5325e2
Alison Golden <alisong779@yahoo.com>
2019-05-21 18:12:05 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -18,17 +18,3 @@ user_id INTEGER,
 project_id INTEGER);
 
 
-
-
-
-
-
-
-
-
-
-
--- **Create a schema based on the following information:**
--- - A project has a title, a category, a funding goal, a start date, and an end date.
--- - A user has a name and an age
--- - A pledge has an amount. It belongs to a user, and it also belongs to a project.
