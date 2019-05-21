fa1554adbe5b0fd598051292c2757f63a3797379
Alison Golden <alisong779@yahoo.com>
2019-05-21 18:14:06 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,10 +1,11 @@
-CREATE TABLE project(
-id INTEGER PRIMARY KEY,
-title TEXT,
-category TEXT,
-funding_goal INTEGER,
-start_date DATE,
-end_date DATE);
+CREATE TABLE projects (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  category TEXT,
+  funding_goal INTEGER,
+  start_date DATE,
+  end_date DATE
+);
 
 CREATE TABLE user(
 id INTEGER PRIMARY KEY,
