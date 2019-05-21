6701e538999484e8b1ac7ba13408995e43aca0fa
Alison Golden <alisong779@yahoo.com>
2019-05-21 18:15:32 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -11,13 +11,13 @@ CREATE TABLE users(
   id INTEGER PRIMARY KEY,
   name TEXT,
   age INTEGER
-  );
+);
 
 CREATE TABLE pledges (
   id INTEGER PRIMARY KEY,
   amount INTEGER
   user_id INTEGER,
   project_id INTEGER
-  );
+);
 
 
